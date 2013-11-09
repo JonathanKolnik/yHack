19075007e651402b83d9d955d91c620d136d9507
Ecako <ecako@bu.edu>
2013-11-08 20:05:10 -0500

    add photo class

---
+++ yHack/Photo.h
@@ -0,0 +1,13 @@
+//
+//  Photo.h
+//  yHack
+//
+//  Created by Elvin Cako on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Photo : NSObject
+
+@end
---
+++ yHack/Photo.m
@@ -0,0 +1,13 @@
+//
+//  Photo.m
+//  yHack
+//
+//  Created by Elvin Cako on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import "Photo.h"
+
+@implementation Photo
+
+@end
