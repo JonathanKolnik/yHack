5294bf9add4f79cbfdbcd25acfd228eb6637c5fa
Jonathan Kolnik <kolnik.com@gmail.com>
2013-11-08 19:21:15 -0500

    Initial Commit

---
+++ yHackTests/yHackTests.m
@@ -0,0 +1,34 @@
+//
+//  yHackTests.m
+//  yHackTests
+//
+//  Created by Jonathan Kolnik on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yHackTests : XCTestCase
+
+@end
+
+@implementation yHackTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
