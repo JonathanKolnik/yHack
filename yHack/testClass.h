5debd0860a3bf3d1dac49d0aa5c813f0d097f82f
KrisNi <krisnii.com@gmail.com>
2013-11-08 19:49:49 -0500

    test commit

---
+++ yHack/testClass.h
@@ -0,0 +1,13 @@
+//
+//  testClass.h
+//  yHack
+//
+//  Created by Kris Ni on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testClass : NSObject
+
+@end
