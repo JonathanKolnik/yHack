c30bbbc433fa6614d69f24fb657622172247d5c1
KrisNi <krisnii.com@gmail.com>
2013-11-08 20:01:42 -0500

    Add you cell class

---
+++ yHack/Cell.h
@@ -0,0 +1,13 @@
+//
+//  Cell.h
+//  yHack
+//
+//  Created by Kris Ni on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cell : NSObject
+
+@end
---
+++ yHack/Cell.m
@@ -0,0 +1,13 @@
+//
+//  Cell.m
+//  yHack
+//
+//  Created by Kris Ni on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+@end
