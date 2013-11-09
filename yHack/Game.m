f3200c3edbc472ca1b029d7c99413173d82a50fb
Ecako <ecako@bu.edu>
2013-11-09 00:48:08 -0500

    Game class

--- yHack/Game.m
+++ yHack/Game.m
@@ -10,4 +10,27 @@
 
 @implementation Game
 
+-(void) addPlayer:(Player *)p{
+    [_players addObject:p];
+}
+
+-(void) deletePlayer:(Player *)p{
+    [_players delete:p];
+}
+
+-(void) findWinner{
+    NSNumber *y = 0;
+    if(_wincount.count == 9){
+        NSMutableArray *playerIndexWins;
+        for(int i = 0; i < playerIndexWins.count; i++){
+            playerIndexWins[i] = y;
+        }
+        NSInteger x;
+        for(NSUInteger i = 0; i < _wincount.count ; i++){
+            x = _wincount[i];
+            playerIndexWins[x] = @([playerIndexWins[x] intValue] + 1);
+        }
+    }
+}
+
 @end
