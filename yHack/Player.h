65a0755084467e14c3932b61b2ef584859fd0db3
Ecako <ecako@bu.edu>
2013-11-08 20:14:14 -0500

    add classes files

---
+++ yHack/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  yHack
+//
+//  Created by Elvin Cako on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@end
---
+++ yHack/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  yHack
+//
+//  Created by Elvin Cako on 11/8/13.
+//  Copyright (c) 2013 bestteamever. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
